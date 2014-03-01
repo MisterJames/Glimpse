b8265d598d2638937456ded7e3c58b3b93f78c56
James Chambers <james@jameschambers.com>
2014-03-01 13:14:05 -0500

    General progress

--- source/Glimpse.Core/Resource/ClientScriptResource.cs
+++ source/Glimpse.Core/Resource/ClientScriptResource.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Glimpse.Core.Extensibility;
 using Glimpse.Core.Framework;
 using Glimpse.Core.ResourceResult;
@@ -12,6 +11,7 @@ namespace Glimpse.Core.Resource
     class ClientScriptResource : IResource, IKey
     {
         internal const string InternalName = "glimpse_clientscripts";
+        internal const string GlimpseRequestId = "glimpse_request_id";
         internal const string OrderParameterName = "order";
 
         public string Name
@@ -26,27 +26,62 @@ namespace Glimpse.Core.Resource
 
         public IResourceResult Execute(IResourceContext context)
         {
-            if (context == null)
+            // verify that the context is populated
+            if (context == null) 
             {
                 throw new ArgumentNullException("context");
             }
 
+            // check for the order parameter
             var orderName = context.Parameters[OrderParameterName];
-            ScriptOrder order;
+            if (string.IsNullOrEmpty(orderName))
+            {
+                throw new ArgumentNullException(OrderParameterName);
+            }
 
-            if(!Enum.TryParse<ScriptOrder>(orderName, out order))
+            // ensure that we can resolve the order name to the enum
+            ScriptOrder order;
+            try
+            {
+                // have to use try..catch instead of Enum.TryParse to support NET35
+                order = (ScriptOrder)Enum.Parse(typeof(ScriptOrder), orderName);
+            }
+            catch (ArgumentException)
             {
                 return new StatusCodeResourceResult(404, string.Format("Could not resolve ScriptOrder for value provided '{0}'.", orderName));
             }
 
-            var config = GlimpseRuntime.Instance.Configuration;
-            var scripts = config.ClientScripts.Where(x => x.Order == order);
+            // heavy lifting time...
+            var glimpseRequestId = context.Parameters[GlimpseRequestId];
+            var configuration = GlimpseRuntime.Instance.Configuration;
+            var scripts = configuration.ClientScripts.Where(x => x.Order == order);
+            var resourceEndpoint = configuration.ResourceEndpoint;
+            var resources = configuration.Resources;
+            var logger = configuration.Logger;
+            var sb = new StringBuilder();
 
-            StringBuilder sb = new StringBuilder();
-
-            foreach (var script in scripts)
+            foreach (var clientScript in scripts)
             {
-                // add matches to the sb
+                
+                var dynamicScript = clientScript as IDynamicClientScript;
+                if (dynamicScript != null)
+                {
+                    
+                    continue;
+                }
+
+                var staticScript = clientScript as IStaticClientScript;
+                if (staticScript != null)
+                {
+                    // note: don't currently have access through configuration for version
+                    // question: do we need version when resolving a static resource?
+                    // suggestion: refactor GetUri to be GetPath...differentiate between local static files
+                    //             and, for example, CDN files that need to be included such as jQuery
+                    staticScript.GetUri("");
+                    continue;
+                }
+
+                logger.Warn(Core.Resources.RenderClientScriptImproperImplementationWarning, clientScript.GetType());
             }
 
             return new CacheControlDecorator(0, CacheSetting.NoCache, new StringResourceResult(@"text/javascript") { Text = sb.ToString() });
